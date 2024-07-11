8e6e54664b68df299f847ecf889b440f3ffd271f
Govind Patel <govind.patel@hostbooks.com>
2024-07-11 11:31:56 +0530

    active navbar

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,10 @@
 import React,{ useContext, useEffect } from 'react'
 import noteContext from '../context/notes/NoteContext'
 const About = () => {
-  const a = useContext(noteContext);
-  useEffect(()=>{
-    a.update();
-     // eslint-disable-next-line
-  },[])
+  
   return (
     <div>
-      This is a About {a.state.name} and he is class {a.state.class}
+      This is a About page
     </div>
   )
 }
